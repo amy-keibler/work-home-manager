3e0a02e6a53e468b6b4b43e63c56feebe52315b7
Amelia Keibler <akeibler@sonatype.com>
2025-08-29 11:27:57 -0400

    Upgrade and comment out latex package

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
